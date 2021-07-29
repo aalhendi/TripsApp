7c584291bc9e9a741518839615f0ba1e5f14a1bf
AyahAshkanani <ayah.ash62@gmail.com>
2021-07-29 17:03:29 +0300

    added triplist in login component

--- components/authentication/Login.js
+++ components/authentication/Login.js
@@ -22,7 +22,7 @@ const Login = ({ navigation }) => {
   });
   const handleSubmit = async () => {
     await authStore.login(user);
-    if (authStore.user) navigation.replace("BakeryList");
+    if (authStore.user) navigation.replace("TripList");
   };
   return (
     <AuthContainer>
