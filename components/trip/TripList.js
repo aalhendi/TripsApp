e3b3db81ec495331fc0a1510d6c0dafd9cac3bac
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-07-29 16:53:27 +0300

    Fix typo

--- components/trip/TripList.js
+++ components/trip/TripList.js
@@ -2,16 +2,24 @@ import React from "react";
 //observer
 import { observer } from "mobx-react";
 //components
-import { Text, Veiw } from "react-native";
+import { Text, View } from "react-native";
 import TripItem from "./TripItem";
 //stores
 import tripStore from "../../stores/tripStore";
+import { Heading } from "native-base";
 
 const TripList = ({ navigation }) => {
   const tripList = tripStore.trips.map((trips) => {
     <TripItem trips={trips} key={trips.id} navigation={navigation} />;
   });
-  return <Veiw>{tripList}</Veiw>;
+  return (
+    <>
+      <View style={{ flex: 1, alignSelf: "center" }}>
+        <Heading>Trip List</Heading>
+      </View>
+      <View>{tripList}</View>
+    </>
+  );
 };
 
 export default observer(TripList);
