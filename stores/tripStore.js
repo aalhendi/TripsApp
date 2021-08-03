b474878a43aedd63de3f2bcd43a26b5ca396cafd
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-08-03 18:37:33 +0300

    Update tripStore.js

--- stores/tripStore.js
+++ stores/tripStore.js
@@ -37,16 +37,13 @@ class TripStore {
       for (const key in updatedTrip) {
         formData.append(key, updatedTrip[key]);
       }
-      console.log(updatedTrip);
       const res = await instance.put(`/trips/${updatedTrip.id}`, formData);
       const myTrip = this.trips.find((myTrip)=> myTrip.id === res.data.id);
       for (const key in myTrip) {
-        //use find for object and edit above for
         myTrip[key] = res.data[key];
       }
 
     } catch (error) {
-      console.log("\n\n ITS NOT WORKING \n\n")
       console.error(error);
     }
   };
